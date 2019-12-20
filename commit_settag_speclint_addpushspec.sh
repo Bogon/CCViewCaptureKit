8781b4bad7629e56c6574a897a466f96cd4fc9a7
mac <zhangqi1@hellogeek.com>
2019-12-20 18:08:37 +0800

    A nice iOS View Capture Library. SwViewCapture could capture all content of UIWebView & UIScrollView.

---
+++ commit_settag_speclint_addpushspec.sh
@@ -0,0 +1,31 @@
+
+# 提交修改，推到主分支
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+git add .
+git commit -m " A nice iOS View Capture Library. SwViewCapture could capture all content of UIWebView & UIScrollView."
+git push -u origin master
+
+echo '+++++++++++++++++++++++++++ commit successful ++++++++++++++++++++'
+
+# 注意需要修改当前的版本号
+echo '+++++++++++++++++++++++++++ pushing tags ++++++++++++++++++++'
+git tag 1.5.0
+git push --tags
+
+echo '+++++++++++++++++++++++++++ pushing successful ++++++++++++++++++++'
+
+# 检查库的语法完整性
+echo '+++++++++++++++++++++++++++ spec lint ++++++++++++++++++++'
+pod spec lint --allow-warnings
+
+echo '+++++++++++++++++++++++++++ spec lint successful ++++++++++++++++++++'
+
+# 提交仓库到本地和公共仓库
+echo '+++++++++++++++++++++++++++ push spec ++++++++++++++++++++'
+pod repo add CCViewCaptureKit https://github.com/Bogon/CCSpec
+pod repo add CCViewCaptureKit https://github.com/CocoaPods/Specs
+pod repo push CCViewCaptureKit CCViewCaptureKit.podspec --sources='https://github.com/Bogon/CCSpec.git, https://github.com/CocoaPods/Specs.git' --allow-warnings
+
+echo '+++++++++++++++++++++++++++ push spec successful ++++++++++++++++++++'
+
+
